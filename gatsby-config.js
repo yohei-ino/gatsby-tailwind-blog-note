653ea91cb817bb86b88e1469785528dac65ca2e9
yohei-ino <yohei.ino.8073@gmail.com>
2021-05-03 02:54:04 +0900

    [update] gatsby-config.js: add fundamental information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  siteMetadata: {
+    title: `gatsby-tailwind-blog-note`,
+    description: ``,
+    author: `@yohei-ino`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/`,
+      },
+    ],
+  },
   plugins: [
     {
       resolve: `gatsby-theme-blog`,
