c67aa14d154bc93b490624e282035080dc6efaeb
yohei-ino <yohei.ino.8073@gmail.com>
2021-05-03 03:31:36 +0900

    [update] Blog: delete socialLinks from home-footer.js

--- src/gatsby-theme-blog/components/home-footer.js
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -1,35 +1,16 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { Themed, css } from "theme-ui"
 
-const Footer = ({ socialLinks }) => (
+const Footer = () => (
   <footer
     css={css({
       mt: 4,
       pt: 3,
     })}
   >
-    © {new Date().getFullYear()}, Powered by
+    © {new Date().getFullYear()}, Created by
     {` `}
-    <Themed.a href="https://www.gatsbyjs.com">Gatsby</Themed.a>
-    {` `}&bull;{` `}
-    {socialLinks
-      ? socialLinks.map((platform, i, arr) => (
-        <Fragment key={platform.url}>
-          <Themed.a
-            href={platform.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {platform.name}
-          </Themed.a>
-          {arr.length - 1 !== i && (
-            <Fragment>
-              {` `}&bull;{` `}
-            </Fragment>
-          )}
-        </Fragment>
-      ))
-      : null}
+    <Themed.a href="https://www.github.com/yohei-ino">yohei-ino</Themed.a>
   </footer>
 )
 export default Footer
