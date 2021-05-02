7b3ccf44999f3bb5e0d3ce8eec1cc701d2d64af3
yohei-ino <yohei.ino.8073@gmail.com>
2021-05-03 03:12:15 +0900

    [update] Home: remove a unnecessary component import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react"
 import Layout from "../components/layout";
-import { ToggleDarkMode } from '../components/ToggleDarkMode'
-import "../styles/index.css";
 
 export default function Home() {
   return (
