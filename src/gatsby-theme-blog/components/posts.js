a413e84ef3862f6f1a68de64774b03e750ca19f7
yohei-ino <yohei.ino.8073@gmail.com>
2021-05-03 03:26:18 +0900

    [update] Blog: shadowing of home-footer.js

--- src/gatsby-theme-blog/components/posts.js
+++ src/gatsby-theme-blog/components/posts.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "./layout"
 import SEO from "gatsby-theme-blog/src/components/seo"
-import Footer from "gatsby-theme-blog/src/components/home-footer"
+import Footer from "./home-footer"
 import PostList from "gatsby-theme-blog/src/components/post-list"
 
 const Posts = ({ location, posts, siteTitle, socialLinks }) => (
