1f0d5f3416e366d415e894194f73828750495cce
yohei-ino <yohei.ino.8073@gmail.com>
2021-05-14 00:13:45 +0900

    [update] Home: change Home design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout";
 
 export default function Home() {
   return (
     <Layout>
       <main>
-        <div className="flex">
-          <p>Hello World!</p>
+        <div className="h-full w-full mx-auto my-0 p-2">
+          <div>
+            <div className="flex justify-center items-center my-5">
+              <Link className="px-2" to="/">Home</Link>
+              <Link className="px-2" to="/about">About</Link>
+              <Link className="px-2" to="/blog">Blog</Link>
+            </div>
+            <p>Hello World!</p>
+          </div>
         </div>
-      </main>
-    </Layout>
+      </main >
+    </Layout >
   )
 }
