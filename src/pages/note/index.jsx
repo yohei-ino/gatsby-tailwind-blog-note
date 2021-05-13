c9b6366f20cc622efd0559a58e143c8a1174dacf
yohei-ino <yohei.ino.8073@gmail.com>
2021-05-14 02:23:03 +0900

    [add] Note: create top page

---
+++ src/pages/note/index.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../../components/layout";
+
+const Note = () => {
+  return (
+    <Layout>
+      <h1>Note</h1>
+    </Layout>
+  )
+}
+
+export default Note
